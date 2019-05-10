ab27a9a3e41a7e581a013a1bbf12830e9d5070c2
Dok92 <Dok92@users.noreply.github.com>
2019-05-10 22:15:41 +0200

    Create & Edit Profile

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import CreateProfile from './components/profile.forms/CreateProfile';
+import EditProfile from './components/profile.forms/EditProfile';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
@@ -35,6 +37,8 @@ const App = () => {
 						<Switch>
 							<Route exact path="/register" component={Register} />
 							<Route exact path="/login" component={Login} />
+							<PrivateRoute exact path="/create-profile" component={CreateProfile} />
+							<PrivateRoute exact path="/edit-profile" component={EditProfile} />
 							<PrivateRoute exact path="/dashboard" component={Dashboard} />
 						</Switch>
 					</section>
