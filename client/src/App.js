021452e3777b5e8f809142ab01f08a9266e9435f
Dok92 <Dok92@users.noreply.github.com>
2019-05-19 12:01:50 +0200

    Finise posts and posts comments

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,8 @@ import Profile from './components/profile/Profile';
 import AddExperience from './components/profile.forms/AddExperience';
 import AddEducation from './components/profile.forms/AddEducation';
 import Dashboard from './components/dashboard/Dashboard';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 // Redux
 import { Provider } from 'react-redux';
@@ -47,6 +49,8 @@ const App = () => {
 							<PrivateRoute exact path="/edit-profile" component={EditProfile} />
 							<PrivateRoute exact path="/add-experience" component={AddExperience} />
 							<PrivateRoute exact path="/add-education" component={AddEducation} />
+							<PrivateRoute exact path="/posts" component={Posts} />
+							<PrivateRoute exact path="/posts/:id" component={Post} />
 							<PrivateRoute exact path="/dashboard" component={Dashboard} />
 						</Switch>
 					</section>
